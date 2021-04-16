e0c559bb7f6a72eecd537fea4b6744f8af5d0a5c
Surya S <77038659+surya-sd@users.noreply.github.com>
2021-04-16 16:16:59 +0530

    Private Route for Campaign Page Added

---
+++ src/Routes/PrivateRoute.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+export const PrivateRoute = ({ children, path, exact = false, redirectPath = "/login", push = false }) => {
+  const { isAuth } = useSelector((state) => state.auth);
+
+  return isAuth ? (
+    <Route path={path} exact={exact}>
+      {children}
+    </Route>
+  ) : (
+    <Redirect to={redirectPath} push={push} />
+  );
+};
