73722ad3a4de41372a0da98942ca3a92ed0eef0c
Surya S <77038659+surya-sd@users.noreply.github.com>
2021-06-08 19:04:45 +0530

    Navbar updated

--- src/Components/Navigation/Navbar.jsx
+++ src/Components/Navigation/Navbar.jsx
@@ -9,51 +9,41 @@ import { logoutSuccess } from "../../Redux/auth/authAction";
 import { fire } from "../Firebase/fire";
 
 export const Navbar = () => {
-	const [drop, setDrop] = useState(true);
-	const [page, setPage] = useState(false);
-	const dispatch = useDispatch();
-	const handledrop = () => {
-		setDrop(!drop);
-		setPage(!page);
-	};
-	const handleLogout = () => {
-		fire.auth().signOut();
-		let payload = {
-			user: false,
-			userName: "",
-		};
-		dispatch(logoutSuccess(payload));
-	};
+  const [drop, setDrop] = useState(true);
+  const [page, setPage] = useState(false);
+  const dispatch = useDispatch();
+  const handledrop = () => {
+    setDrop(!drop);
+    setPage(!page);
+  };
+  const handleLogout = () => {
+    fire.auth().signOut();
+    let payload = {
+      user: false,
+      userName: "",
+    };
+    dispatch(logoutSuccess(payload));
+  };
 
-	const { isAuth, userName } = useSelector((state) => state.auth);
-	// console.log(isAuth, userName);
-	return (
-		<div className={styles.nav1}>
-			<nav className={styles.nav}>
-				<div className={styles.div1}>
-					<NavLink to="/" className={styles.logo}>
-						INDIEGOGO
-					</NavLink>
-					<NavLink
-						to="/"
-						className={styles.navitemleft}
-						style={{ paddingRight: "7px" }}
-						onClick={handledrop}
-					>
-						Explore{" "}
-						{drop ? (
-							<FontAwesomeIcon icon={faChevronDown} />
-						) : (
-							<FontAwesomeIcon icon={faChevronUp} />
-						)}
-					</NavLink>
-					<NavLink to="/about-what-we-do" className={styles.navitemleft}>
-						What we do
-					</NavLink>
-					
-					{/* <FontAwesomeIcon icon={faSearch} className={styles.navitemleft} /> */}
-				</div>
-				{/* <div className={styles.div2}>
+  const { isAuth, userName } = useSelector((state) => state.auth);
+  // console.log(isAuth, userName);
+  return (
+    <div className={styles.nav1}>
+      <nav className={styles.nav}>
+        <div className={styles.div1}>
+          <NavLink to="/" className={styles.logo}>
+            INDIEGOGO
+          </NavLink>
+          <NavLink to="/" className={styles.navitemleft} style={{ paddingRight: "7px" }} onClick={handledrop}>
+            Explore {drop ? <FontAwesomeIcon icon={faChevronDown} /> : <FontAwesomeIcon icon={faChevronUp} />}
+          </NavLink>
+          <NavLink to="/about-what-we-do" className={styles.navitemleft}>
+            What we do
+          </NavLink>
+
+          {/* <FontAwesomeIcon icon={faSearch} className={styles.navitemleft} /> */}
+        </div>
+        {/* <div className={styles.div2}>
           <NavLink to="/how-it-work" className={styles.navitemright}>
             For Entrepreneurs
           </NavLink>
@@ -70,7 +60,6 @@ export const Navbar = () => {
       </nav>
       {page ? <Dropdown /> : null}
         </div> */}
-<<<<<<< HEAD
         <div className={styles.div2}>
           <NavLink to="/how-it-work" className={styles.navitemright}>
             For Entrepreneurs
@@ -102,41 +91,4 @@ export const Navbar = () => {
       {page ? <Dropdown /> : null}
     </div>
   );
-=======
-				<div className={styles.div2}>
-					<NavLink to="/how-it-work" className={styles.navitemright}>
-						For Entrepreneurs
-					</NavLink>
-					<NavLink to="/start-a-campaign" className={styles.navitemright1}>
-						Start a campaign
-					</NavLink>
-					{isAuth ? (
-						<>
-							<NavLink to="#" className={styles.navitemright}>
-								{userName}
-							</NavLink>
-							<NavLink
-								to="#"
-								onClick={() => handleLogout()}
-								className={styles.navitemright}
-							>
-								logout
-							</NavLink>
-						</>
-					) : (
-						<>
-							<NavLink to="/login" className={styles.navitemright}>
-								Log in
-							</NavLink>
-							<NavLink to="/login" className={styles.navitemright}>
-								Sign Up
-							</NavLink>
-						</>
-					)}
-				</div>
-			</nav>
-			{page ? <Dropdown /> : null}
-		</div>
-	);
->>>>>>> 0c2d5f8150f10f13dffa6568a25a0bad979e6717
 };
